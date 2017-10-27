b6e3d3a6c1d1ea337d71f1315ff5c623d370202e
jekyllx <anand.pal2050@gmail.com>
2017-10-27 14:32:08 +0530

    commit

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -7,3 +7,28 @@ $(window).on('load',function() {
 		$('.preloader').remove();
 	});
 });
+
+
+//****************
+//  owl-carousel
+//****************
+$(".owl-carousel").owlCarousel();
+$('.owl-carousel').owlCarousel({
+	  items:6,
+	  loop:true,
+    margin:10,
+		autoplay:true,
+	  autoplayTimeout:1000,
+	  autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
